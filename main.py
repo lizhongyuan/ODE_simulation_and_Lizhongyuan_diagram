4fc97f7c614126611e82adbfc839d9fae4d37ba3
lizhongyuan <lizhongyuan@bimsop.com>
2024-02-04 15:12:00 +0800

    add recursive func

--- main.py
+++ main.py
@@ -42,11 +42,18 @@ def string_merge(str1, str2):
 
 def gen_table_header_arr(event_indexes):
     count = len(event_indexes)
-    combinations_and_pivots_array = [[]]
+    # combinations_and_pivots_array = [[]]
+    combinations_and_pivots_array = []
+
+    zero_event_idx = '0'
+    zero_items = ['0']
+    combination_array = [zero_event_idx]
+    binomial_theorem_array = [zero_items]
 
     for i in range(count):
         if i == 0:
-            cur_items = combinations_and_pivots_array[0]
+            # cur_items = combinations_and_pivots_array[0]
+            cur_items = []
             for j in range(count):
                 cur_item = {
                     'combination': event_indexes[j],
@@ -69,10 +76,8 @@ def gen_table_header_arr(event_indexes):
                     }
                     cur_items.insert(len(cur_items), cur_item)
 
-            combinations_and_pivots_array.insert(len(combinations_and_pivots_array), cur_items)
+        combinations_and_pivots_array.insert(len(combinations_and_pivots_array), cur_items)
 
-    combination_array = []
-    binomial_theorem_array = []
     for i in range(count):
         binomial_theorem_array.append([])
         for j in range(len(combinations_and_pivots_array[i])):
@@ -80,14 +85,77 @@ def gen_table_header_arr(event_indexes):
             combination_array.append(cur_value)
             binomial_theorem_array[i].append(cur_value)
 
-    combination_array.insert(0, '0')
-    binomial_theorem_array.insert(0, ['0'])
-
-    # return combinations_and_pivots
     return {
         'combination_array': combination_array,
         'binomial_theorem_array': binomial_theorem_array
     }
+
+
+def gen_table_header_arr2(event_indexes):
+    print('recur version')
+    count = len(event_indexes)
+    combinations_and_pivots_array = [[]]
+
+    zero_event_idx = '0'
+    zero_items = ['0']
+    combination_array = [zero_event_idx]
+    binomial_theorem_array = [zero_items]
+
+    recur(combinations_and_pivots_array, combination_array, binomial_theorem_array, count, count - 1, event_indexes)
+
+    return {
+        'combination_array': combination_array,
+        'binomial_theorem_array': binomial_theorem_array
+    }
+
+
+def recur(combinations_and_pivots_array, combination_array, binomial_theorem_array, count, i, event_indexes):
+    print('recur')
+
+    if i == 0:
+        cur_items = combinations_and_pivots_array[i]
+        for j in range(count):
+            cur_item = {
+                'combination': event_indexes[j],
+                'pivot': j
+            }
+            cur_items.insert(len(cur_items), cur_item)
+
+        # combinations_and_pivots_array.insert(len(combinations_and_pivots_array), cur_items)
+
+        binomial_theorem_array.append([])
+        for j in range(len(combinations_and_pivots_array[0])):
+            cur_value = combinations_and_pivots_array[i][j]['combination']
+            combination_array.append(cur_value)
+            # binomial_theorem_array[i].append(cur_value)
+            binomial_theorem_array[i + 1].append(cur_value)
+
+        print(1234)
+    else:
+        recur(combinations_and_pivots_array, combination_array, binomial_theorem_array, count, i - 1, event_indexes)
+
+        pre_items = combinations_and_pivots_array[i - 1]
+        cur_items = []
+
+        for j in range(len(pre_items)):
+
+            pre_combination = pre_items[j]['combination']
+
+            for k in range(pre_items[j]['pivot'] + 1, count):
+                cur_combination = pre_combination + event_indexes[k]
+                cur_item = {
+                    'combination': cur_combination,
+                    'pivot': k
+                }
+                cur_items.insert(len(cur_items), cur_item)
+
+        combinations_and_pivots_array.insert(len(combinations_and_pivots_array), cur_items)
+
+        binomial_theorem_array.append([])
+        for j in range(len(combinations_and_pivots_array[i])):
+            cur_value = combinations_and_pivots_array[i][j]['combination']
+            combination_array.append(cur_value)
+            binomial_theorem_array[i + 1].append(cur_value)
 
 
 def build_data_frame(data_frame):
@@ -109,67 +177,51 @@ def color_up_data_frame(data_frame, plt):
 
     fig, ax = plt.subplots()
 
-    # color_table = ax.table(cellText=data_frame.values,
-    #                        colLabels=data_frame.columns,
-    #                        rowLabels=data_frame.index,
-    #                        loc="center")
-
-    color_arr = []
+    color_matrix = []
     for row_idx in range(data_frame.index.size):
-        cur_row_arr = []
+        row_colors = []
         for col_idx in range(data_frame.columns.size):
             if data_frame.values[row_idx][col_idx] == 'ERR':
-                # color_table.get_celld()[(row_idx + 1, col_idx)].set_facecolor('#222222')
-                cur_row_arr.append(0.0)
+                row_colors.append(0)  # set black
             else:
-                # color_table.get_celld()[(row_idx + 1, col_idx)].set_facecolor('#D40000')
-                cur_row_arr.append(1.1)
-        color_arr.append(cur_row_arr)
+                row_colors.append(100)  # set color
+        color_matrix.append(row_colors)
 
-    color_arr = np.array(color_arr)
-    print(color_arr)
     # ax.imshow(X=color_arr, cmap=plt.cm.get_cmap('gist_heat'))
     cmap = colors.ListedColormap(['#080402', '#D03D33'])
-    ax.imshow(X=np.array(color_arr), cmap=cmap)
-    # ax.imshow(X=np.array(color_arr), cmap=mpl.colormaps['copper'])
+    ax.imshow(X=np.array(color_matrix), cmap=cmap)
 
     ax.set_xticks(np.arange(data_frame.columns.size), labels=data_frame.columns)
     ax.set_yticks(np.arange(data_frame.index.size), labels=data_frame.index)
 
     fig.tight_layout()
-    plt.show()
-    #
-    # for row_idx in range(data_frame.index.size):
-    #     for col_idx in range(data_frame.columns.size):
-    #         if data_frame.values[row_idx][col_idx] == 'ERR':
-    #             color_table.get_celld()[(row_idx + 1, col_idx)].set_facecolor('#222222')
-    #         else:
-    #             color_table.get_celld()[(row_idx + 1, col_idx)].set_facecolor('#D40000')
-    #
+
 
 if __name__ == '__main__':
-    # str1 = 'abc'
-    str1 = 'ABC'
+    str1 = 'A'
+    str2 = 'AB'
+    str3 = 'ABC'
     # a, b, c, ab, ac, bc, abc
 
-    str2 = 'ABCD'
+    str4 = 'ABCD'
     # a, b, c, d, ab, ac, ad, bc, bd, cd, abc, abd, bcd, abcd
 
-    str3 = 'ABCDE'
+    str5 = 'ABCDE'
     # a, b, c, d, e,
     # ab, ac, ad, ae, bc, bd, be, cd, ce, de,
     # abc, abd, abe, acd, ace, ade, bcd, bce, bde, cde,
     # abcd, abce, abde, acde, bcde
     # abcde
 
-    str4 = 'ABCDEF'
-    str5 = 'ABCDEFG'
-    str6 = 'ABCDEFGH'
-    str7 = 'ABCDEFGHIJK'    # 再增加字符等待的时间会很久
+    str6 = 'ABCDEF'
+    str7 = 'ABCDEFG'
+    str8 = 'ABCDEFGH'
+    str9 = 'ABCDEFGHI'
+    str10 = 'ABCDEFGHIJ'
+    str11 = 'ABCDEFGHIJK'  # 再增加字符等待的时间会很久
 
-    # arr = test(str1)
-    # arr = test(str2)
-    res = gen_table_header_arr(str2)
+    # res = gen_table_header_arr(str3)
+    res = gen_table_header_arr2(str4)
     headers = res['combination_array']
 
     data_frame = init_data_frame(headers)
@@ -177,6 +229,6 @@ if __name__ == '__main__':
     build_data_frame(data_frame)
     color_up_data_frame(data_frame, plt)
 
-    # plt.show()
+    plt.show()
 
-    print(data_frame)
+    # print(data_frame)
