b1bce5fa8a3e889f9d80a127268f8bfa7a728b05
cyberdash_yuan <cyberdash@163.com>
2024-11-05 00:35:27 +0800

    add sth

---
+++ simulations/twoTupleT.py
@@ -0,0 +1,25 @@
+from simulations.structure import TwoTupleT
+
+
+def get_min_1_of_2tuple_T(p_2tupleT: TwoTupleT) -> object:
+    if p_2tupleT is None:
+        return None
+
+    min_first = p_2tupleT[0].first()
+    for _2tuple in p_2tupleT:
+        if min_first > _2tuple.first():
+            min_first = _2tuple.first()
+    return min_first
+
+
+def get_max_2_of_2tuple_T(p_2tupleT: TwoTupleT) -> object:
+    if p_2tupleT is None:
+        return None
+
+    max_second = p_2tupleT[0].second()
+    for _2tuple in p_2tupleT:
+        if max_second < _2tuple.second():
+            max_second = _2tuple.second()
+    return max_second
+
+# todo: 定义24
---
+++ test/test.2tupleT.py
@@ -0,0 +1,23 @@
+from simulations.structure import TwoTuple, TwoTupleT
+from simulations.twoTupleT import get_min_1_of_2tuple_T, get_max_2_of_2tuple_T
+
+if __name__ == '__main__':
+
+    twoT1 = TwoTuple([1, 2])
+    twoT2 = TwoTuple([3, 4])
+    twoT3 = TwoTuple([1, 3])
+    twoT4 = TwoTuple([2, 4])
+
+
+    _2tuple_T_1 = TwoTupleT([twoT1, twoT2])
+    _2tuple_T_2 = TwoTupleT([twoT3, twoT4])
+
+    min_1_of_1 = get_min_1_of_2tuple_T(_2tuple_T_1)
+    max_2_of_1 = get_max_2_of_2tuple_T(_2tuple_T_1)
+    min_1_of_2 = get_min_1_of_2tuple_T(_2tuple_T_2)
+    max_2_of_2 = get_max_2_of_2tuple_T(_2tuple_T_2)
+
+    print(f'min_1_of_1: {min_1_of_1}')
+    print(f'max_2_of_1: {max_2_of_1}')
+    print(f'min_1_of_2: {min_1_of_2}')
+    print(f'max_2_of_2: {max_2_of_2}')
