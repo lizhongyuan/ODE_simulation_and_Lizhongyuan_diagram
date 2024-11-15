91ee1b188c219cabbc6c03e8fea7c0cdbf91a691
lizhongyuan <lizhongyuan@bimsop.com>
2024-11-15 18:13:54 +0800

    add sth

--- simulations/unfeasible.py
+++ simulations/unfeasible.py
@@ -5,7 +5,7 @@
 """
 
 from typing import List
-from simulations.structure import _2TupleTS
+from simulations.structure import _2TupleTS, _2TupleT
 
 
 def get_feasible_2tuple_TS(p_entire_DI_2tuple_TS: _2TupleTS,
@@ -31,11 +31,27 @@ def get_feasible_2tuple_TS(p_entire_DI_2tuple_TS: _2TupleTS,
         raise ValueError("p_op_idx_T and p_unfeasible_idx_T must have same items !")
 
     # todo:
-    # idx_trans_dict = {}
-    # for op_idx in p_op_idx_T:
+    idx_trans_dict = {}
+    for op_idx in range(0, len(p_op_idx_T)):
+        for idx in range(0, len(p_unfeasible_idx_T)):
+            if p_unfeasible_idx_T[idx] == p_op_idx_T[op_idx]:
+                idx_trans_dict[idx] = op_idx
+
+    print(str(idx_trans_dict))
+
+    op_order_unfeasible_DI_2tuple_TS = _2TupleTS([])
+    for unfeasible_DI_2tuple_T in p_unfeasible_DI_2tuple_TS:
+        op_order_unfeasible_DI_2tuple_list = []
+        for idx in range(0, len(p_unfeasible_idx_T)):
+            op_order_unfeasible_DI_2tuple_list.append(unfeasible_DI_2tuple_T[idx_trans_dict[idx]])
+        op_order_unfeasible_DI_2tuple_TS.add(_2TupleT(op_order_unfeasible_DI_2tuple_list))
+
+    print(op_order_unfeasible_DI_2tuple_TS)
     #
     # pass
 
+
+# todo: 验证
 
 op_idx_T = (2, 1, 3)
 # unfeasible_idx_T = (2, 1, 4)
