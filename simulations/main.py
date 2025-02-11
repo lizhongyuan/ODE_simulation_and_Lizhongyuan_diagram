01e2eecd4a5fdb5add245256716d94b12cab1cba
cyberdash_yuan <cyberdash@163.com>
2025-02-11 16:40:33 +0800

    add sth

--- simulations/main.py
+++ simulations/main.py
@@ -15,12 +15,9 @@ from simulations.test.pdieOP.test_add import (test_complete_sequential_addition_
 from simulations.test.pdieOP.test_multi import (test_complete_sequential_multiplication_1,
                                                 test_complete_sequential_multiplication_2,
                                                 test_complete_sequential_multiplication_3)
-from simulations.test.base.test_f_feasible_DI_2tuple_TS import test_f_feasible_DI_2tuple_TS
 
 
 if __name__ == '__main__':
-
-#    test_f_feasible_DI_2tuple_TS()
 
     test_complete_sequential_addition_1()
 
